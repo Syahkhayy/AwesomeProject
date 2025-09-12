dbeb41328973778d31933e0974a45f054951eab0
Syahmi Khairul <syahmi2001@gmail.com>
2025-09-12 11:49:41 +0800

    Border Styling

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   return (
     <SafeAreaView>
-      <Text>Hi, I just completed my first React Native Assignment!</Text>
+      <View style={{
+        borderWidth: 1,
+        marginTop: 20,
+        borderColor: '#0fa328ff',
+        borderRadius: 10,
+        padding: 10,
+      }}>
+        <Text>Hi, I just completed my first React Native Assignment!</Text>
+      </View>
     </SafeAreaView>
   );
 }
