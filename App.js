3e16a6ea2f8adf5677b64102255227726cbc4a17
Syahmi Khairul <syahmi2001@gmail.com>
2025-09-17 16:44:46 +0800

    Custom Hook

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, View } from 'react-native';
 import style from './components/MyText/style';
 import MyText from './components/MyText/MyText';
-import { ThemeContext } from './context/ThemeContext';
-import HomeScreen from './components/HomeScreen/HomeScreen';
+import { Button, Text } from 'react-native';
+import useToggle from './customHooks/UseToggle';
+
 
 function App() {
-
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+  const [isOn, toggleIsOn] = useToggle(false);
 
   return (
-    <ThemeContext.Provider value={isDarkMode}>
-      <SafeAreaView style={style.mainView}>
-        <View style={{ backgroundColor: isDarkMode ? '#222222' : '#ffffff', height: 100 }}>
-          <MyText index={1} />
-        </View>
-        <HomeScreen />
-        <Button title={"Switch Mode"} onPress={toggleTheme} />
-      </SafeAreaView>
-    </ThemeContext.Provider>
+    <SafeAreaView style={style.mainView}>
+      <Text>{isOn ? 'ON' : 'OFF'}</Text>
+      <Button title={'Toggle'} onPress={toggleIsOn} />
+    </SafeAreaView>
 
 
   );
